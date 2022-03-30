21225e6d56da8c8dfc73bb11331ab9bf6cc03d2f
given-woo <2210220@bsis.hs.kr>
2022-03-30 17:30:15 +0900

    delete banner

--- src/index.js
+++ src/index.js
@@ -7,10 +7,6 @@ import Form from "./pages/Form"
 const rootElement = document.getElementById("root");
 ReactDOM.render(
   <BrowserRouter>
-    <div class="banner">
-      <img src="https://i.ibb.co/w4t0Hfg/bamboo.png"></img>
-      <p>대나무<span>숲</span></p>
-    </div>
     <Routes>
       <Route exact path="/" element={<Home />}></Route>
       <Route exact path="/Form" element={<Form />}></Route>
