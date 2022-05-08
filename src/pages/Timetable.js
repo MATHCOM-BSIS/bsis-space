29ccf7d0d72fe12fa7ca849b40d7079c724802ae
given-woo <2210220@bsis.hs.kr>
2022-05-08 20:10:37 +0900

    added fetchOnline function

--- src/pages/Timetable.js
+++ src/pages/Timetable.js
@@ -1,5 +1,21 @@
-import React, { useState } from "react";
+import "../timetable.css";
+
+import React, { useEffect, useState } from "react";
 
 export default function Timetable() {
-    return(<p>test</p>);
+    const [data, setData] = React.useState({});
+    const fetchOnline = async () => {
+        const response = await fetch(
+            "https://open.neis.go.kr/hub/hisTimetable?KEY=3c07c8b644464b768a20bc4370a8e842&Type=json&ATPT_OFCDC_SC_CODE=C10&SD_SCHUL_CODE=7150532"
+        );
+        const data = await response.json();
+        setData(data);
+    };
+    fetchOnline();
+
+    return(
+        <main>
+            <pre>{JSON.stringify(data, null, "  ")}</pre>
+        </main>
+    );
 }
